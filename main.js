4050eabd020c5ec228096afe5541482f5d9c26a7
Faiz Shaikh <faiz.22010206@viit.ac.in>
2023-01-26 22:45:23 +0530

    only animation and mobile slide bar left

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 const downloadbtn_text=document.querySelectorAll(".downloadbtn-text");
+const downloadOrSignupBtn=document.getElementById("download-or-signup-btn");
 const width=window.innerWidth;
 if(width>1024){
     downloadbtn_text.forEach((node)=>{
         node.innerHTML="Download for Windows"
-    })
+    });
+    downloadOrSignupBtn.innerHTML="Sign up"
 }
+
+const openInBrowserBtn=document.getElementById("openinbrower-btn");
+const downloadOrOpenSec=document.getElementById("download-or-open-sec");
+const openInBrowserSection=document.getElementById("openinbrower-section");
+
+
+openInBrowserBtn.addEventListener("click",()=>{
+    downloadOrOpenSec.style.display="none";
+    openInBrowserSection.style.display="block";
+})
+console.log(openInBrowserBtn);
+console.log(openInBrowserSection);
+console.log(downloadOrOpenSec);
